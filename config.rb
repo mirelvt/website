59e5e516ed116f45949a756486cb6b0bad552b70
Mirella van Teulingen <mirella@dreamsolution.nl>
2014-03-14 19:08:54 +0100

    Clean up config, update text + add intro text on fronteers page:
    - Remove example configuration
    - Add js dir to the config
    - Set environment on development, now we can use firecompass
    - Update class names
    - Add font-smoothing

--- config.rb
+++ config.rb
@@ -1,28 +1,9 @@
-# Require any additional compass plugins here.
-
-# Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "html/css"
 sass_dir = "scss"
 images_dir = "html/img"
-# javascripts_dir = "javascripts"
+javascripts_dir = "html/js"
 
-# You can select your preferred output style here (can be overridden via the command line):
-#output_style = :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-
-# Debug sass
-sass_options = {:debug_info => true}
-output_style = :expanded
+# environment = :production
+environment = :development
+output_style = (environment == :production) ? :compressed : :expanded
