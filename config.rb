6bcf806e5a041117eebb36a82f1f7214f07a758c
Mirella van Teulingen <mirella@dreamsolution.nl>
2014-03-29 20:24:06 +0100

    Remove makefile, it is replaced by grunt, remove config.rb i no longer use compass.

--- config.rb
+++
@@ -1,9 +0,0 @@
-http_path = "/"
-css_dir = "html/css"
-sass_dir = "scss"
-images_dir = "html/img"
-javascripts_dir = "html/js"
-
-# environment = :production
-environment = :development
-output_style = (environment == :production) ? :compressed : :expanded
