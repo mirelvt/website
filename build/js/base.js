afca87f9c85f39d5f61a723af89330936e831d2e
Mirella van Teulingen <mailenm@mirellavanteulingen.nl>
2014-05-25 16:47:39 +0200

    Create menu for mobile:
    - Add menu-icon to the icon set
    - Create base.js file
    - Create MenuComponent
    - Style the mobile menu

---
+++ build/js/base.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  function MenuComponent(container) {
+    var menu_title = container.querySelector('.menu_title');
+
+    menu_title.addEventListener('click', function(evt) {
+      toggleMenu();
+    }, false);
+
+    function toggleMenu() {
+      if (container.hasAttribute('class')) {
+          container.removeAttribute('class')
+      }
+      else {
+        container.setAttribute('class', 'show');
+      }
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var nav = document.querySelector('nav');
+    new MenuComponent(nav);
+  }, false);
+
+})();
