73ac381ee43ad53b8aee4b14dc030f3e247aaadb
mirelvt <mailenm@mirellavanteulingen.nl>
2015-02-01 14:34:54 +0100

    Style submenu:
    - Position the submenu
    - Implement basic styling
    - Add hover + selected styles.
    - Position content
    - Add class column-content to the pages who has a submenu

--- build/js/base.js
+++ build/js/base.js
@@ -1,1 +1,23 @@
-!function(){"use strict";function a(a){function b(){a.classList.contains("show")?a.classList.remove("show"):a.classList.add("show")}var c=a.querySelector(".menu-title");c.addEventListener("click",b,!1)}function b(){var b=document.querySelector('[role="navigation"]');a(b),FastClick.attach(document.body)}document.addEventListener("DOMContentLoaded",b,!1)}();
+(function() {
+  'use strict';
+
+  function menuComponent(container) {
+    var menu_title = container.querySelector('.menu-title');
+
+    menu_title.addEventListener('click', toggleMenu, false);
+
+    function toggleMenu() {
+      var dropdown_menu = container.classList.contains('show') ? container.classList.remove('show') :
+                          container.classList.add('show');
+    }
+  }
+
+  function onDocumentLoaded() {
+    var nav = document.querySelector('[role="navigation"]');
+    menuComponent(nav);
+
+    FastClick.attach(document.body);
+  }
+
+  document.addEventListener('DOMContentLoaded', onDocumentLoaded, false);
+})();
