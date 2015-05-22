75bf515ce9f30a6b4bdf827d2d411a9fa5c130ae
Mirella van Teulingen <mirella@dreamsolution.nl>
2015-05-22 17:08:45 +0200

    Use rem instead of px for viewport mqs

--- build/js/about_me.js
+++ build/js/about_me.js
@@ -1,1 +1,43 @@
-!function(){"use strict";function a(a){function b(a,b){a.setAttribute("src","img/"+b),a.setAttribute("class","photo-side"),a.setAttribute("alt","Concentrated at work")}var c=document.createElement("img"),d=document.createElement("img");if(window.matchMedia("(max-width: 862px)").matches)for(var e=a.getElementsByClassName("photo-side"),f=0;f<e.length;f++)a.removeChild(e[f]);else 1===a.children.length&&(b(c,"aboutme-left.jpg"),b(d,"aboutme-right.jpg"),a.insertBefore(d,a.firstChild),a.insertBefore(c,a.firstChild))}function b(){new a(document.querySelector(".photos"))}document.addEventListener("DOMContentLoaded",b,!1),window.addEventListener("resize",b,!1)}();
+(function() {
+  'use strict';
+
+    //@Constructor PhotosComponent {{
+    /*
+    * When the viewport reach a certain size, 2 more images are
+    * inserted. By a small viewport the 2 side images are removed.
+    */
+  function PhotosComponent(container) {
+    var img_left = document.createElement('img'),
+        img_right = document.createElement('img');
+
+    if (window.matchMedia('(max-width: 862px)').matches) {
+        var photos = container.getElementsByClassName('photo-side');
+        for (var i = 0; i < photos.length; i++) {
+          container.removeChild(photos[i]);
+        }
+    } else {
+        if (container.children.length === 1) {
+            createImage(img_left, 'aboutme-left.jpg');
+            createImage(img_right, 'aboutme-right.jpg');
+
+            container.insertBefore(img_right, container.firstChild);
+            container.insertBefore(img_left, container.firstChild);
+        }
+    }
+
+    // Create image element
+    function createImage(elm, src) {
+      elm.setAttribute('src', 'img/' + src);
+      elm.setAttribute('class', 'photo-side');
+      elm.setAttribute('alt','Concentrated at work');
+    }
+  }
+  // }}
+
+  function onDocumentLoaded() {
+    new PhotosComponent(document.querySelector('.photos'));
+  }
+
+  document.addEventListener('DOMContentLoaded', onDocumentLoaded, false);
+  window.addEventListener('resize', onDocumentLoaded, false);
+})();
