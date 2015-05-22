546e7cf9561ed49cc17f561d718f401f947f88b0
Mirella van Teulingen <mirella@dreamsolution.nl>
2015-05-22 13:51:18 +0200

    Update articles html, tweak about_me.js file

--- public/js/about_me.js
+++ public/js/about_me.js
@@ -1,52 +1,41 @@
 (function() {
   'use strict';
 
-  var img_container = document.querySelector('.photos');
-
-  // @Constructor AboutMeComponent {{
-  function AboutMeComponent(container) {
+    //@Constructor PhotosComponent {{
+    /*
+    * When the viewport reach a certain size, 2 more images are
+    * inserted. By a small viewport the 2 side images are removed.
+    */
+  function PhotosComponent(container) {
     var img_left = document.createElement('img'),
         img_right = document.createElement('img');
 
+    if (window.matchMedia('(max-width: 862px)').matches) {
+        var photos = container.getElementsByClassName('photo-side');
+        for (var i = 0; i < photos.length; i++) {
+          container.removeChild(photos[i]);
+        }
+    } else {
+        if (container.children.length === 1) {
+            createImage(img_left, 'aboutme-left.jpg');
+            createImage(img_right, 'aboutme-right.jpg');
 
-    (function() {
-      loadImages();
-      deleteImages();
-    })();
+            container.insertBefore(img_right, container.firstChild);
+            container.insertBefore(img_left, container.firstChild);
+        }
+    }
 
     // Create image element
-    function loadImage(elm, src) {
+    function createImage(elm, src) {
       elm.setAttribute('src', 'img/' + src);
       elm.setAttribute('class', 'photo-side');
       elm.setAttribute('alt','Concentrated at work');
-    }
-
-    // Load left and right images when viewport > 743 and has only 1 image.
-    function loadImages() {
-      if (window.innerWidth >= 743 && container.children.length === 1) {
-        loadImage(img_left, 'aboutme-left.jpg');
-        loadImage(img_right, 'aboutme-right.jpg');
-
-        container.insertBefore(img_right, container.firstChild);
-        container.insertBefore(img_left, container.firstChild);
-      }
-    }
-
-    // Remove left and right images when viewport < 743
-    function deleteImages() {
-      if (window.innerWidth < 743) {
-        var photos = img_container.getElementsByClassName('photo-side');
-
-        for (var i = 0; i < photos.length; i++) {
-            img_container.removeChild(photos[i]);
-        }
-      }
     }
   }
   // }}
 
   function onDocumentLoaded() {
-    new AboutMeComponent(img_container);
+    new PhotosComponent(document.querySelector('.photos'));
   }
 
   document.addEventListener('DOMContentLoaded', onDocumentLoaded, false);
