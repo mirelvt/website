7d070de680a80203f75dea95f7ed3e57b339543c
Mirella van Teulingen <mailenm@mirellavanteulingen.nl>
2015-08-06 15:52:18 +0200

    Use OLOO (Objects Linking to Other Objects) style for adding the 2 photos.

--- public/js/about_me.js
+++ public/js/about_me.js
@@ -1,41 +1,37 @@
 (function() {
   'use strict';
 
-    //@Constructor PhotosComponent {{
-    /*
-    * When the viewport reach a certain size, 2 more images are
-    * inserted. By a small viewport the 2 side images are removed.
-    */
-  function PhotosComponent(container) {
-    var img_left = document.createElement('img'),
-        img_right = document.createElement('img');
+    // Photo Widget Object
+    var PhotosWidget = {
+        elm_create: function(container) {
+            this.elm = document.createElement('img');
+            container.insertBefore(this.elm, container.firstChild);
+        },
+        elm_attrs: function(img_src) {
+            this.elm.className = 'photo-side';
+            this.elm.src = 'img/' + img_src;
+            this.elm.alt = 'At work';
+        },
+    };
 
-    if (window.matchMedia('(max-width: 862px)').matches) {
-        var photos = container.getElementsByClassName('photo-side');
-        for (var i = 0; i < photos.length; i++) {
-          container.removeChild(photos[i]);
+    var Photo = Object.create(PhotosWidget);
+    Photo.setup = function(wrapper, img_src) {
+        if (window.matchMedia('(max-width: 862px)').matches) {
+            var photos = wrapper.getElementsByClassName('photo-side');
+            for (var i = 0; i < photos.length; i++) {
+                wrapper.removeChild(photos[i]);
+            }
+        } else if (wrapper.children.length < 3) {
+            this.elm_create(wrapper);
+            this.elm_attrs(img_src);
         }
-    } else {
-        if (container.children.length === 1) {
-            setImageAttrs(img_left, 'aboutme-left.jpg');
-            setImageAttrs(img_right, 'aboutme-right.jpg');
-
-            container.insertBefore(img_right, container.firstChild);
-            container.insertBefore(img_left, container.firstChild);
-        }
-    }
-
-    // Set image attributes
-    function setImageAttrs(elm, src) {
-      elm.className = 'photo-side';
-      elm.src = 'img/' + src;
-      elm.alt = 'At work';
-    }
-  }
-  // }}
+    };
 
   function onDocumentLoaded() {
-    new PhotosComponent(document.querySelector('.photos'));
+    var img_left = Object.create(Photo);
+    var img_right = Object.create(Photo);
+    img_left.setup(document.querySelector('.photos'), 'aboutme-left.jpg');
+    img_right.setup(document.querySelector('.photos'), 'aboutme-right.jpg');
   }
 
   document.addEventListener('DOMContentLoaded', onDocumentLoaded, false);
